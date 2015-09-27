7bdbaea2b3aed76e558b496957f6ddf004688930
devchakraborty <devchakraborty@users.noreply.github.com>
2015-09-27 13:36:54 -0400

    Sept 2015 version

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+var autoprefixer = require('autoprefixer');
+var webpack = require('webpack');
+
+var env = process.env.NODE_ENV;
+
+module.exports = {
+	entry: {
+		javascript: './src/js/index.js',
+		html: './src/index.html'
+	},
+	output: {
+		path: './build',
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.(html)$/, loader: 'file?name=[name].[ext]'
+			},
+			{
+				test: /\.(png|jpg|svg|eot|ttf|woff|woff2|pdf)$/, loader: 'url?limit=8192'
+			},
+			{
+				test: /\.js$/, exclude: /node_modules/, loader: (env=='production'?'':'source-map!')+'babel'
+			},
+			{
+				test: /\.(css|scss)$/, loader: 'style!css!postcss!sass'
+			}
+		]
+	},
+	devServer: {
+		contentBase: './build',
+		historyApiFallback: true,
+		port: 7000
+	},
+	postcss: function() {
+		return [autoprefixer];
+	},
+	plugins: env == 'production' ? [new webpack.optimize.UglifyJsPlugin({minimize:true, comments:false})] : []
+};
+
+if (env == 'production')
+	module.exports.devtool = 'source-map';
